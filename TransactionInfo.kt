869c87a887f9b49222344af62d49795ed8043131
C5FR7Q <qwe2rty1@gmail.com>
2019-01-17 23:00:32 +0700

    Use BaseFragment instead of Navigation- one in TransactionInfo.

--- TransactionInfo.kt
+++ TransactionInfo.kt
@@ -1,7 +1,7 @@
 package com.example.qwe2r.myapplication
 
 data class TransactionInfo(
-        val fragment: NavigationFragment,
+        val fragment: BaseFragment,
         val tag: String,
         val saveToBackStack: Boolean
 )
